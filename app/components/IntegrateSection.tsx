0c48cd2596437703b29f09bc3d525b3562e1a020
madhurMongia <madhurrajput1010@gmail.com>
2025-06-24 00:43:40 +0530

    refactor: update comment syntax in IntegrateSection component for consistency and clarity

--- app/components/IntegrateSection.tsx
+++ app/components/IntegrateSection.tsx
@@ -29,7 +29,7 @@ const IntegrateSection = () => {
                   <span className="text-yellow-300">IProofOfHumanity</span>{" "}
                   <span className="text-white">{'{'}</span>
                 </div>
-                <div className="ml-2 text-gray-400 mb-1">// Core verification functions</div>
+                <div className="ml-2 text-gray-400 mb-1">{`// Core verification functions`}</div>
                 <div className="ml-2 mb-1">
                   <span className="text-orange-400">function</span>{" "}
                   <span className="text-blue-300">isHuman</span>
@@ -87,7 +87,7 @@ const IntegrateSection = () => {
                   <span className="text-white">);</span>
                 </div>
 
-                <div className="ml-2 text-gray-400 mb-1">// Detailed information function</div>
+                <div className="ml-2 text-gray-400 mb-1">{`// Detailed information function`}</div>
                 <div className="ml-2 mb-1">
                   <span className="text-orange-400">function</span>{" "}
                   <span className="text-blue-300">getHumanityInfo</span>
@@ -103,38 +103,38 @@ const IntegrateSection = () => {
                 <div className="ml-4 mb-1">
                   <span className="text-orange-400">bool</span>{" "}
                   <span className="text-white">vouching,</span>
-                  <span className="text-gray-400 ml-14">// Is this humanity currently vouching for someone</span>
+                  <span className="text-gray-400 ml-14">{`// Is this humanity currently vouching for someone`}</span>
                 </div>
                 <div className="ml-4 mb-1">
                   <span className="text-orange-400">bool</span>{" "}
                   <span className="text-white">pendingRevocation,</span>
-                  <span className="text-gray-400 ml-5">// Is there a pending revocation request</span>
+                  <span className="text-gray-400 ml-5">{`// Is there a pending revocation request`}</span>
                 </div>
                 <div className="ml-4 mb-1">
                   <span className="text-orange-400">uint48</span>{" "}
                   <span className="text-white">nbPendingRequests,</span>
-                  <span className="text-gray-400 ml-3">// Number of pending requests in challenging phase</span>
+                  <span className="text-gray-400 ml-3">{`// Number of pending requests in challenging phase`}</span>
                 </div>
                 <div className="ml-4 mb-1">
                   <span className="text-orange-400">uint40</span>{" "}
                   <span className="text-white">expirationTime,</span>
-                  <span className="text-gray-400 ml-6">// When the humanity expires</span>
+                  <span className="text-gray-400 ml-6">{`// When the humanity expires`}</span>
                 </div>
                 <div className="ml-4 mb-1">
                   <span className="text-orange-400">address</span>{" "}
                   <span className="text-white">owner,</span>
-                  <span className="text-gray-400 ml-17">// Current owner address</span>
+                  <span className="text-gray-400 ml-17">{`// Current owner address`}</span>
                 </div>
                 <div className="ml-4 mb-1">
                   <span className="text-orange-400">uint256</span>{" "}
                   <span className="text-white">nbRequests</span>
-                  <span className="text-gray-400 ml-10">// Total number of requests made</span>
+                  <span className="text-gray-400 ml-10">{`// Total number of requests made`}</span>
                 </div>
                 <div className="ml-2 mb-4">
                   <span className="text-white">);</span>
                 </div>
 
-                <div className="ml-2 text-gray-400 mb-1">// Statistics and request information</div>
+                <div className="ml-2 text-gray-400 mb-1">{`// Statistics and request information`}</div>
                 <div className="ml-2 mb-1">
                   <span className="text-orange-400">function</span>{" "}
                   <span className="text-blue-300">getClaimerRequestId</span>
